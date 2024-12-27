e47584c6f85442af470de2464582ab233af50d8a
Maan-py <mluqmaan22@gmail.com>
2024-12-27 13:44:56 +0700

    fix(TopNav): update app logo path

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -4,7 +4,7 @@ function TopNav() {
   return (
     <nav className="bg-[#1d232a] z-50 h-16 flex items-center justify-between px-10 border-b border-gray-800 sticky top-0">
       <div className="flex flex-row">
-        <img src="../public/app-logo.svg" alt="app logo" className="mr-2 mt-1" width={25} />
+        <img src="/app-logo.svg" alt="app logo" className="mr-2 mt-1" width={25} />
         <h1 className="font-bold text-white-500 text-xl">Quran</h1>
       </div>
       <div className="flex items-center gap-2">
