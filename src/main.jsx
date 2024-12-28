1f346f8f3e0400fc9ca6c077d1203646d08aacc7
Maan-py <mluqmaan22@gmail.com>
2024-12-28 21:58:19 +0700

    refactor(imports): update import paths for pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import Home from "./pages/Home";
-import SurahDetail from "./pages/SurahDetail";
+import Home from "./Pages/Home";
+import SurahDetail from "./Pages/SurahDetail";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
