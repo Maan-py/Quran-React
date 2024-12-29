4035e028b848f61125dfc1d323bc3f0dfb686339
Maan-py <mluqmaan22@gmail.com>
2024-12-29 13:17:32 +0700

    feat(surah): add transliteration of ayat

--- src/components/RightSection.jsx
+++ src/components/RightSection.jsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const RightSection = ({ selectedSurahName, selectedSurahNumber }) => {
+  const { id } = useParams();
   const [detailAyat, setdetailAyat] = useState([]);
 
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_BASE_URL}/verses/by_chapter/${selectedSurahNumber}?fields=text_uthmani&per_page=286&translations=33`)
+    fetch(`${import.meta.env.VITE_API_BASE_URL}/verses/by_chapter/${id}?fields=text_uthmani&per_page=286&translations=33&words=true`)
       .then((response) => response.json())
       .then((data) => setdetailAyat(data.verses))
       .catch((e) => {
         throw new Error(e);
       });
-  }, [selectedSurahNumber]);
+  }, [id]);
 
   return (
     <section className="sm:basis-3/4 w-full overflow-auto bg-slate-800 flex flex-col">
@@ -30,8 +32,17 @@ const ItemAyat = ({ detailAyat }) => {
     <div>
       {detailAyat.map((detail) => (
         <div key={detail.id} className="w-full px-10 py-4 border-b border-slate-600 p-2">
-          <div className="w-full h-full flex justify-end">
+          <div className="w-full h-full flex flex-col justify-end">
             <h3 className="text-2xl font-bold font-surah block text-right break-words mb-2">{detail.text_uthmani}</h3>
+            <div className="inline-flex justify-end">
+              {/* Menambahkan transliterasi */}
+              {detail.words &&
+                detail.words.map((word, index) => (
+                  <div key={index} className="text-lg text-right">
+                    <p className="text-sm text-gray-400">{word.transliteration.text}</p>
+                  </div>
+                ))}
+            </div>
           </div>
           <span className="mb-2" dangerouslySetInnerHTML={{ __html: detail.translations[0].text }} />
         </div>
